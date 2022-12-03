495c6f3c07ad9ae59ae9583a11d73700c97756c2
shaunmoini <primary.axos@dralias.com>
2022-12-03 17:27:39 -0500

    feat: add date picker and display date fact
    - implemented using mui StaticDatePicker and numbers api
    - restructured src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 
-function App() {
+// import type {} from '@mui/x-date-pickers/themeAugmentation';
+// import type {} from '@mui/x-date-pickers-pro/themeAugmentation';
+import './css/app.css';
+
+const App = () => {
+  const [startDate, setStartDate] = React.useState<Dayjs>(dayjs(new Date()));
+  const [currentFact, setCurrentFact] = React.useState<string | null>(null);
+
+  // request new date fact on date change
+  React.useEffect(() => {
+    fetch(`http://numbersapi.com/${startDate.month() + 1}/${startDate.date()}/date`)
+      .then(res => res.text())
+      .then(data => setCurrentFact(data))
+      .catch(e => console.log(e))
+  }, [startDate])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='flex-container'>
+      <h1 id='title'>Fact Calendar</h1>
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+          showToolbar={false}
+          value={startDate}
+          onChange={(newValue) => setStartDate(newValue!)}
+          renderInput={(params) => <TextField {...params} />}
+          componentsProps={{ actionBar: { sx: { display: 'none' } } }}
+        />
+      </LocalizationProvider>
+
+      <p id='currentFact'>{currentFact}</p>
     </div>
   );
 }
